8c44a57668b7c834e986fbdd2ad7ba76e1429b55
roberthsu2003 <roberthsu2003@gmail.com>
2024-05-28 15:08:12 +0800

    modify

--- window_layout/index1.py
+++ window_layout/index1.py
@@ -6,6 +6,15 @@ class Window(tk.Tk):
         super().__init__(**kwargs)
         self.title("pack1")
 
+        btn1:ttk.Button = ttk.Button(self,text="Top")
+        btn1.pack()
+
+        btn2:ttk.Button = ttk.Button(self,text="Middle")
+        btn2.pack()
+
+        btn3:ttk.Button = ttk.Button(self,text="Bottom")
+        btn3.pack()
+
 
 if __name__ == '__main__':
     window:Window = Window()
