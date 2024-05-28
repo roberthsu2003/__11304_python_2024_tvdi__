35048fea5dc909b0c472f244ac009589503ddafa
roberthsu2003 <roberthsu2003@gmail.com>
2024-05-28 16:22:40 +0800

    modify

---
+++ window_layout/index4.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import ttk
+
+class Window(tk.Tk):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.title("pack1")
+        self.geometry('300x200')
+        
+        ttk.Button(self,text="Top").pack(fill='both',expand=1)
+
+        ttk.Button(self,text="Middle").pack(fill='both',expand=1)  
+
+        ttk.Button(self,text="Bottom").pack(fill='both',expand=1)
+        
+
+
+if __name__ == '__main__':
+    window:Window = Window()
+    window.mainloop()
