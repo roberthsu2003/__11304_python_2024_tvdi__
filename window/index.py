011a3caa6de6f929cfe5491ff811373cb5bf6313
roberthsu2003 <roberthsu2003@gmail.com>
2024-05-28 11:47:00 +0800

    modify

--- window/index.py
+++ window/index.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+
 def get_names() -> list[str]:
     with open('names.txt',encoding='utf-8') as file:
         content:str = file.read()
@@ -6,6 +8,7 @@ def get_names() -> list[str]:
 
 if __name__ == '__main__':
     names:list[str] = get_names()
-    print(names)
+    window:tk.Tk = tk.Tk()
+    window.mainloop()
 
 
