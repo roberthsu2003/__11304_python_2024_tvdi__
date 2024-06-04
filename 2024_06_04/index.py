9bdc7db5365dff13fd53afaf2542965bcc202c99
roberthsu2003 <roberthsu2003@gmail.com>
2024-06-04 15:32:15 +0800

    modify

--- 2024_06_04/index.py
+++ 2024_06_04/index.py
@@ -19,11 +19,23 @@ class Window(ThemedTk):
         title_frame.pack(ipadx=100,ipady=30,padx=10,pady=10)
 
         func_frame = ttk.Frame(self,style='Top.TFrame',borderwidth=1,relief='groove')
-        ttk.Button(func_frame,text="AQI品質最好的5個").pack(side='left',expand=True)
-        ttk.Button(func_frame,text="AQI品質最差的5個").pack(side='left',expand=True)
-        ttk.Button(func_frame,text="pm2.5品質最好的5個").pack(side='left',expand=True)
-        ttk.Button(func_frame,text="pm2.5品質最好的5個").pack(side='left',expand=True)
+        ttk.Button(func_frame,text="AQI品質最好的5個",command=self.click1).pack(side='left',expand=True)
+        ttk.Button(func_frame,text="AQI品質最差的5個",command=self.click2).pack(side='left',expand=True)
+        ttk.Button(func_frame,text="pm2.5品質最好的5個",command=self.click3).pack(side='left',expand=True)
+        ttk.Button(func_frame,text="pm2.5品質最好的5個",command=self.click4).pack(side='left',expand=True)
         func_frame.pack(ipadx=100,ipady=30,padx=10,pady=10)
+
+    def click1(self):
+        print("click1")
+    
+    def click2(self):
+        print("click2")
+
+    def click3(self):
+        print("click3")
+    
+    def click4(self):
+        print("click4")
 
 
 
