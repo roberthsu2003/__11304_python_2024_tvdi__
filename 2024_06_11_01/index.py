453bb60434cee0bbbaa67e3203ca6728f11a19fc
roberthsu2003 <roberthsu2003@gmail.com>
2024-06-11 10:58:00 +0800

    modify

--- 2024_06_11_01/index.py
+++ 2024_06_11_01/index.py
@@ -11,6 +11,7 @@ class Window(ThemedTk):
         self.resizable(False,False)
         style = ttk.Style()
         style.configure('input.TFrame',background='#ffffff')
+        style.configure('press.TButton',font=('arial',16))
 
         titleFrame = ttk.Frame(self)
         title_label = ttk.Label(self, text="BMI計算器", font=("Arial", 20))
@@ -20,25 +21,32 @@ class Window(ThemedTk):
         input_frame = ttk.Frame(self,style='Input.TFrame')
         # 姓名
         label_name = ttk.Label(input_frame, text="姓名:")
-        label_name.grid(row=0, column=0, padx=5, pady=5)
+        label_name.grid(row=0, column=0, padx=5, pady=5,sticky=tk.E)
 
         entry_name = ttk.Entry(input_frame)
         entry_name.grid(row=0, column=1, padx=5, pady=5)
 
         # 身高體重
         label_height = ttk.Label(input_frame, text="身高 (cm):")
-        label_height.grid(row=1, column=0, padx=5, pady=5)
+        label_height.grid(row=1, column=0, padx=5, pady=5,sticky=tk.E)
 
         entry_height = ttk.Entry(input_frame)
         entry_height.grid(row=1, column=1, padx=5, pady=5)
 
         label_weight = ttk.Label(input_frame, text="體重 (kg):")
-        label_weight.grid(row=2, column=0, padx=5, pady=5)
+        label_weight.grid(row=2, column=0, padx=5, pady=5,sticky=tk.E)
 
         entry_weight = ttk.Entry(input_frame)
-        entry_weight.grid(row=2, column=1, padx=5, pady=5)        
+        entry_weight.grid(row=2, column=1, padx=5, pady=5)    
 
         input_frame.pack(pady=10,padx=30)
+
+        button_calculate = ttk.Button(self, text="計算", command=self.show_bmi_result,style='press.TButton')
+        button_calculate.pack(side=tk.RIGHT,padx=(0,35),pady=10,ipadx=10,ipady=15)
+
+    
+    def show_bmi_result(self):
+        print("計算")
 
 
 
