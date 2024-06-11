ab223fffaa06cb12ee72cfe816562aa80903c4dc
roberthsu2003 <roberthsu2003@gmail.com>
2024-06-11 14:20:09 +0800

    modify

--- 2024_06_11_01/tools.py
+++ 2024_06_11_01/tools.py
@@ -13,33 +13,33 @@ class CustomMessagebox(Dialog):
 
     def body(self, master):
         # 創建對話框主體。返回應具有初始焦點的控件。
-        input_frame = ttk.Frame(self,style='Input.TFrame')
+        contain_frame = ttk.Frame(master,style='Input.TFrame')
         # 姓名
-        label_name = ttk.Label(input_frame, text="姓名:")
+        label_name = ttk.Label(contain_frame, text="姓名:")
         label_name.grid(row=0, column=0, padx=5, pady=5,sticky=tk.E)
 
-        self.value_name = ttk.Label(input_frame,text=self.name)
+        self.value_name = ttk.Label(contain_frame,text=self.name)
         self.value_name.grid(row=0, column=1, padx=5, pady=5)
 
         # bmi
-        label_bmi = ttk.Label(input_frame, text="BMI值:")
+        label_bmi = ttk.Label(contain_frame, text="BMI值:")
         label_bmi.grid(row=1, column=0, padx=5, pady=5,sticky=tk.E)
 
-        self.value_height = ttk.Label(input_frame,text=f'{self.bmi:.2f}')
+        self.value_height = ttk.Label(contain_frame,text=f'{self.bmi:.2f}')
         self.value_height.grid(row=1, column=1, padx=5, pady=5)
 
         # status
-        label_status = ttk.Label(input_frame, text="狀態:")
+        label_status = ttk.Label(contain_frame, text="狀態:")
         label_status.grid(row=2, column=0, padx=5, pady=5,sticky=tk.E)
 
-        self.value_status = ttk.Label(input_frame,text=self.status)
+        self.value_status = ttk.Label(contain_frame,text=self.status)
         self.value_status.grid(row=2, column=1, padx=5, pady=5)
 
         #advice
-        label_advice = ttk.Label(input_frame, text="建議:")
+        label_advice = ttk.Label(contain_frame, text="建議:")
         label_advice.grid(row=3, column=0, padx=5, pady=5,sticky=tk.E)
 
-        self.value_advice = ttk.Label(input_frame,text=self.advice)
+        self.value_advice = ttk.Label(contain_frame,text=self.advice)
         self.value_advice.grid(row=3, column=1, padx=5, pady=5)   
 
-        input_frame.pack(pady=10,padx=30)
+        contain_frame.pack(pady=10,padx=30)
