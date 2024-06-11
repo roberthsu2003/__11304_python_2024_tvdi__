8ef92eff52319440b114d956bedd8336f98c2b33
arminlinster <31943995+arminlinster@users.noreply.github.com>
2024-06-11 14:16:24 +0800

    m0611 1416

---
+++ homework/林正銘/issue216/tools.py
@@ -0,0 +1,45 @@
+from tkinter.simpledialog import Dialog
+from tkinter import ttk
+from tkinter import Misc
+import tkinter as tk
+
+class CustomMessagebox(Dialog):    
+    def __init__(self, parent:Misc, title:str,name:str,bmi:float,status:str,advice:str):        
+        self.name = name
+        self.bmi = bmi
+        self.status = status
+        self.advice = advice
+        super().__init__(parent=parent, title=title)
+
+    def body(self, master):
+        # 創建對話框主體。返回應具有初始焦點的控件。
+        input_frame = ttk.Frame(self,style='Input.TFrame')
+        # 姓名
+        label_name = ttk.Label(input_frame, text="姓名:")
+        label_name.grid(row=0, column=0, padx=5, pady=5,sticky=tk.E)
+
+        self.value_name = ttk.Label(input_frame,text=self.name)
+        self.value_name.grid(row=0, column=1, padx=5, pady=5)
+
+        # bmi
+        label_bmi = ttk.Label(input_frame, text="BMI值:")
+        label_bmi.grid(row=1, column=0, padx=5, pady=5,sticky=tk.E)
+
+        self.value_height = ttk.Label(input_frame,text=f'{self.bmi:.2f}')
+        self.value_height.grid(row=1, column=1, padx=5, pady=5)
+
+        # status
+        label_status = ttk.Label(input_frame, text="狀態:")
+        label_status.grid(row=2, column=0, padx=5, pady=5,sticky=tk.E)
+
+        self.value_status = ttk.Label(input_frame,text=self.status)
+        self.value_status.grid(row=2, column=1, padx=5, pady=5)
+
+        #advice
+        label_advice = ttk.Label(input_frame, text="建議:")
+        label_advice.grid(row=3, column=0, padx=5, pady=5,sticky=tk.E)
+
+        self.value_advice = ttk.Label(input_frame,text=self.advice)
+        self.value_advice.grid(row=3, column=1, padx=5, pady=5)   
+
+        input_frame.pack(pady=10,padx=30)
