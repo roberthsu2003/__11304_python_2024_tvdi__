8afcae16cf3da1914756bc0f128f81f5b39835bd
roberthsu2003 <roberthsu2003@gmail.com>
2024-06-14 10:09:25 +0800

    modify

--- 2024_06_14_01/index.py
+++ 2024_06_14_01/index.py
@@ -8,7 +8,8 @@ class Window(ThemedTk):
         super().__init__(theme=theme,**kwargs)
         self.title('台北市YouBike2.0及時資料')
         try:
-            self.__data = data.load_data()
+            #self.__data = data.load_data()
+            pass
         except Exception as e:
             messagebox.showwarning(title='警告',message=str(e))
         
@@ -18,6 +19,29 @@ class Window(ThemedTk):
         mainFrame = ttk.Frame(borderwidth=1,relief='groove')
         ttk.Label(mainFrame,text="台北市YouBike2.0及時資料",font=('arial',25)).pack()
         tableFrame = ttk.Frame(mainFrame)
+        columns = ('first_name', 'last_name', 'email')
+        tree = ttk.Treeview(tableFrame, columns=columns, show='headings')
+        # define headings
+        tree.heading('first_name', text='First Name')
+        tree.heading('last_name', text='Last Name')
+        tree.heading('email', text='Email')
+
+        # generate sample data
+        contacts = []
+        for n in range(1, 100):
+            contacts.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
+        
+        # add data to the treeview
+        for contact in contacts:
+            tree.insert('', tk.END, values=contact)
+        
+        tree.grid(row=0, column=0, sticky='nsew')
+
+        scrollbar = ttk.Scrollbar(tableFrame, orient=tk.VERTICAL, command=tree.yview)
+        tree.configure(yscroll=scrollbar.set)
+        scrollbar.grid(row=0, column=1, sticky='ns')
+
+
         tableFrame.pack(expand=True,fill=tk.BOTH)
         mainFrame.pack(expand=True,fill=tk.BOTH,padx=10,pady=10)
 
@@ -25,7 +49,8 @@ class Window(ThemedTk):
 
     @property
     def data(self)->list[dict]:
-        return self.__data
+        pass
+        #return self.__data
 
 
 def main():
