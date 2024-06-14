0528e71b7ed3a1e62803421d85bdabe8e8b36d8d
roberthsu2003 <roberthsu2003@gmail.com>
2024-06-14 16:42:40 +0800

    modify

--- 2024_06_14_01/tools.py
+++ 2024_06_14_01/tools.py
@@ -23,6 +23,12 @@ class CustomMessagebox(Dialog):
         map_widget.pack()
         marker = map_widget.set_position(self.site.lat, self.site.lng,marker=True) #台北市位置
         marker.set_text(f'{self.site.sarea}\n{self.site.sna}\n總車輛:{self.site.total}\n可借:{self.site.rent_bikes}\n可還:{self.site.retuen_bikes}')
+        start_point = self.site.lat+0.0005,self.site.lng+0.0005
+        end_point = start_point[0], start_point[1]-0.001
+        down_point = end_point[0]-0.001,end_point[1]
+        left_point = down_point[0], down_point[1] + 0.001
+        up_point = left_point[0]+0.001, left_point[1]
+        path = map_widget.set_path([start_point,end_point,down_point,left_point,up_point])
         map_widget.set_zoom(20) #設定顯示大小
         map_frame.pack(expand=True,fill='both')
         #===================
