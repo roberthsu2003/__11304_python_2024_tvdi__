974ffea320c2e4c1f0f8212b7d96c954f85e0c0a
roberthsu2003 <roberthsu2003@gmail.com>
2024-06-14 10:26:54 +0800

    modify

--- 2024_06_14_01/index.py
+++ 2024_06_14_01/index.py
@@ -8,8 +8,7 @@ class Window(ThemedTk):
         super().__init__(theme=theme,**kwargs)
         self.title('台北市YouBike2.0及時資料')
         try:
-            #self.__data = data.load_data()
-            pass
+            self.__data = data.load_data()
         except Exception as e:
             messagebox.showwarning(title='警告',message=str(e))
         
@@ -18,39 +17,40 @@ class Window(ThemedTk):
     def _display_interface(self):
         mainFrame = ttk.Frame(borderwidth=1,relief='groove')
         ttk.Label(mainFrame,text="台北市YouBike2.0及時資料",font=('arial',25)).pack()
+        #=================================
         tableFrame = ttk.Frame(mainFrame)
-        columns = ('first_name', 'last_name', 'email')
+        columns = ('sna', 'sarea', 'mday','ar','total','rent_bikes','retuen_bikes')
         tree = ttk.Treeview(tableFrame, columns=columns, show='headings')
         # define headings
-        tree.heading('first_name', text='First Name')
-        tree.heading('last_name', text='Last Name')
-        tree.heading('email', text='Email')
+        tree.heading('sna', text='站點')
+        tree.heading('sarea', text='行政區')
+        tree.heading('mday', text='時間')
+        tree.heading('ar', text='地址')
+        tree.heading('total', text='總數')
+        tree.heading('rent_bikes', text='可借')
+        tree.heading('retuen_bikes', text='可還')
 
-        # generate sample data
-        contacts = []
-        for n in range(1, 100):
-            contacts.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
+        
         
         # add data to the treeview
-        for contact in contacts:
-            tree.insert('', tk.END, values=contact)
+        for site in self.data:
+            tree.insert('', tk.END,
+                        values=(site['sna'],site['sarea'],site['mday'],site['ar'],site['total'],site['rent_bikes'],site['retuen_bikes']))
         
         tree.grid(row=0, column=0, sticky='nsew')
 
         scrollbar = ttk.Scrollbar(tableFrame, orient=tk.VERTICAL, command=tree.yview)
         tree.configure(yscroll=scrollbar.set)
         scrollbar.grid(row=0, column=1, sticky='ns')
-
-
         tableFrame.pack(expand=True,fill=tk.BOTH)
+        #======================================
         mainFrame.pack(expand=True,fill=tk.BOTH,padx=10,pady=10)
 
 
 
     @property
     def data(self)->list[dict]:
-        pass
-        #return self.__data
+        return self.__data
 
 
 def main():
