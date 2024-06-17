d4a1aae61c2a915cf545237293f7948792a5f894
roberthsu2003 <roberthsu2003@gmail.com>
2024-06-17 14:09:33 +0800

    modify

--- 2024_06_17/index.py
+++ 2024_06_17/index.py
@@ -88,21 +88,25 @@ class PieChartFrame(ttk.Frame):
         self.configure({'borderwidth':2,'relief':'groove'})
         #self.config({'borderwidth':2,'relief':'groove'})        
         #self['borderwidth'] = 2
-        #self['relief'] = 'groove' 
-        canvas = tk.Canvas(self)
-        canvas.create_line(15, 30, 200,30)
-        canvas.create_line(300,35, 300, 200,dash=(8,2))
-        canvas.create_line(55,85,155,85,105,180,55,85)
-        canvas.pack(expand=True,fill='both')
+        #self['relief'] = 'groove'         
 
     @property
     def infos(self)->None:
         return None
 
     @infos.setter
-    def infos(self,data:list[list]) -> None:
-        print('資料傳進來了')
-        print(data)
+    def infos(self,datas:list[list]) -> None:
+        for data in datas:
+            sitename:str = data[0]
+            area:str = data[1]
+            info_time:str = data[2]
+            address:str = data[3]
+            total:int = data[4]
+            rents:int = data[5]
+            returns:int = data[6]
+            oneFrame = ttk.Frame(self)
+            ttk.Label(oneFrame,text=area).grid(row=0,column=0)
+            oneFrame.pack(side='left')  
         
 
 
