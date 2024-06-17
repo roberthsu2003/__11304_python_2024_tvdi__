385427d54d77922e362c3c032b959978bab8cabc
roberthsu2003 <roberthsu2003@gmail.com>
2024-06-17 14:47:41 +0800

    modify

--- 2024_06_17/index.py
+++ 2024_06_17/index.py
@@ -81,7 +81,9 @@ class Window(ThemedTk):
         
             
 
-
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 class PieChartFrame(ttk.Frame):
     def __init__(self,master:Misc,**kwargs):
         super().__init__(master=master,**kwargs)
@@ -93,7 +95,8 @@ class PieChartFrame(ttk.Frame):
     @property
     def infos(self)->None:
         return None
-
+    
+    
     @infos.setter
     def infos(self,datas:list[list]) -> None:
         for w in self.winfo_children():
@@ -129,7 +132,27 @@ class PieChartFrame(ttk.Frame):
             ttk.Label(oneFrame,text="可還:").grid(row=5,column=0,sticky='e')
             ttk.Label(oneFrame,text=str(returns)).grid(row=5,column=1,sticky='w')
 
-            oneFrame.pack(side='left',expand=True,fill='both')  
+            def func(pct, allvals):
+                absolute = int(np.round(pct/100.*np.sum(allvals)))
+                return f"{absolute:d}pcs - {pct:.1f}%"
+
+            values = [rents, returns]
+            labels = ['Rend','Return']
+            colors = ['green','red']
+            figure = plt.figure(figsize=(5,5),dpi=72)
+            axes = figure.add_subplot()
+            axes.pie(values,colors=colors,
+                    labels=labels,
+                    labeldistance=0.4,
+                    shadow=True,
+                    autopct=lambda pct: func(pct, values),
+                    textprops=dict(color="white"))
+            
+            canvas = FigureCanvasTkAgg(figure,oneFrame)
+            canvas.draw()
+            canvas.get_tk_widget().grid(row=6,column=0,columnspan=2)
+
+            oneFrame.pack(side='left',expand=True,fill='both') 
         
 
 
