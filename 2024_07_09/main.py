4c0bea14fb0f38e15d393c983549ea2cf28a8984
roberthsu2003 <roberthsu2003@gmail.com>
2024-07-09 11:17:06 +0800

    modify

--- 2024_07_09/main.py
+++ 2024_07_09/main.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def index():
+    return "<h1>我的主題</h1>\n<h2>職能發展學院</h2>"
+
+@app.route('/hello')
+def hello():
+    return '<h1>Hello, World</h1>'
