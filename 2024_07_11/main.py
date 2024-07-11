4c062b60ca8a9fc8a2b618e45ec5bb0fe24de301
roberthsu2003 <roberthsu2003@gmail.com>
2024-07-11 16:21:22 +0800

    modify

--- 2024_07_11/main.py
+++ 2024_07_11/main.py
@@ -5,7 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     #print(list(map(lambda value:value[0],data.get_areas())))
-    
+    selected_area = request.args.get('area')
     areas = [tup[0] for tup in data.get_areas()]
-    print(areas)
-    return render_template('index.html.jinja',areas=areas)
+    if  selected_area is None:
+        print("第一次進入")      
+        return render_template('index.html.jinja',areas=areas)
+    else:
+        print(selected_area)
+        return render_template('index.html.jinja',areas=areas)
+    
+    
+    
