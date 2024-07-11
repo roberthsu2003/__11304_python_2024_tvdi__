aaeecaa31939f7b81f479cc7f5c941b90a17ef28
roberthsu2003 <roberthsu2003@gmail.com>
2024-07-11 14:31:22 +0800

    modify

---
+++ 2024_07_11/data.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import psycopg2
+import os
+load_dotenv()
+
+
+
+def get_areas():
+    conn = psycopg2.connect(os.environ['POSTGRESQL_TOKEN'])
+    with conn:
+        with conn.cursor() as cursor:
+            sql ='''
+            SELECT DISTINCT sarea
+            FROM youbike;
+            '''
+
+            cursor.execute(sql)
+            print(cursor.fetchall())
+    conn.close()
