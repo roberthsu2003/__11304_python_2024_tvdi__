e7bf8279e346b9bacf5e30420fece685ece9c2ca
roberthsu2003 <roberthsu2003@gmail.com>
2024-07-11 16:34:40 +0800

    modify

--- 2024_07_11/main.py
+++ 2024_07_11/main.py
@@ -7,12 +7,9 @@ def index():
     #print(list(map(lambda value:value[0],data.get_areas())))
     selected_area = request.args.get('area')
     areas = [tup[0] for tup in data.get_areas()]
-    if  selected_area is None:
-        print("第一次進入")      
-        return render_template('index.html.jinja',areas=areas)
-    else:
-        print(selected_area)
-        return render_template('index.html.jinja',areas=areas)
+    selected_area = '士林區' if selected_area is None else selected_area  
+        
+    return render_template('index.html.jinja',areas=areas,show_area=selected_area)    
     
     
     
