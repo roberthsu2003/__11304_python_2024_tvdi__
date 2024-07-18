2d200968c16d9a4b1817e13abe17b7508b9dcf5f
LariHong <gccbs05591job@gmail.com>
2024-07-18 08:59:09 +0800

    24

--- homework/股票預測_08周廷恩_21張煇昇_24洪振傑/features/feature.py
+++ homework/股票預測_08周廷恩_21張煇昇_24洪振傑/features/feature.py
@@ -2,6 +2,7 @@ import pandas as pd
 from .moving_average import Calculate_Moving_Average
 from .relative import Calculate_Rsi
 from .bollinger import Calculate_Bollinger_Bands
+from .macd import Calculate_Macd
 
 
 class Feature():
@@ -9,6 +10,7 @@ class Feature():
         self._Calculate_Moving_Average =Calculate_Moving_Average
         self._Calculate_Rsi =Calculate_Rsi
         self._Calculate_Bollinger_Bands =Calculate_Bollinger_Bands
+        self._Calculate_Macd=Calculate_Macd
 
     @property
     def Calculate_Moving_Average(self):
@@ -18,4 +20,7 @@ class Feature():
         return self._Calculate_Rsi
     @property
     def Calculate_Bollinger_Bands(self):
+        return self._Calculate_Bollinger_Bands
+    @property
+    def Calculate_Macd(self):
         return self._Calculate_Bollinger_Bands
