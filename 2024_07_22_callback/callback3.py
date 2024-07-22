2d599f711dfb15781351f4c91669c316348af565
roberthsu2003 <roberthsu2003@gmail.com>
2024-07-22 14:19:33 +0800

    modify

--- 2024_07_22_callback/callback3.py
+++ 2024_07_22_callback/callback3.py
@@ -12,13 +12,27 @@ app.layout = html.Div([
         value=5
     ),
     html.Table([
-        html.Tr([html.Td(['x',html.Sup(2),html.Td(id='square')])]),
-        html.Tr([html.Td(['x',html.Sup(3),html.Td(id='cube')])]),
-        html.Tr([html.Td(['2',html.Sup('x'),html.Td(id='twos')])]),
-        html.Tr([html.Td(['3',html.Sup(3),html.Td(id='threes')])]),
-        html.Tr([html.Td(['x',html.Sup('x'),html.Td(id='x^x')])]),
-        ])
+        html.Tr([
+            html.Td(['x',html.Sup(2)]),
+            html.Td(id='square')])
+            ,
+        html.Tr([
+            html.Td(['x',html.Sup(3)]),
+            html.Td(id='cube')])
+            ,
+        html.Tr([
+            html.Td([2,html.Sup('x')]),
+            html.Td(id='twos')])
+            ,
+        html.Tr([
+            html.Td(['3',html.Sup('x')]),
+            html.Td(id='threes')])
+            ,
+        html.Tr([
+            html.Td(['x',html.Sup('x')]),
+            html.Td(id='x^x')])
     ])
+])
 
 @callback(
     Output('square','children'),
