40753497c90c2856ed8c53154fbe86559eb352bb
roberthsu2003 <roberthsu2003@gmail.com>
2024-07-22 15:49:22 +0800

    modify

---
+++ 2024_07_22_callback/callback5.py
@@ -0,0 +1,17 @@
+from dash import Dash,html,dcc,Input,Output,callback
+import data
+
+app = Dash(__name__)
+
+areas = [tup[0] for tup in data.get_areas()]
+app.layout = html.Div([
+    dcc.Dropdown(
+       options = areas,
+       value = areas[0],
+       id='areas'
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
