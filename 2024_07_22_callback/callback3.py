9eb7e179ad5f7b94bf60b5d0154919a402e3b534
roberthsu2003 <roberthsu2003@gmail.com>
2024-07-22 13:43:17 +0800

    modify

--- 2024_07_22_callback/callback3.py
+++ 2024_07_22_callback/callback3.py
@@ -5,6 +5,22 @@ print(external_stylesheets)
 
 app = Dash(__name__,external_stylesheets=external_stylesheets)
 
+app.layout = html.Div([
+    dcc.Input(
+        id='num-multi',
+        type='number',
+        value=5
+    ),
+    html.Table([
+        html.Tr([html.Td(['x',html.Sup(2),html.Td(id='square')])]),
+        html.Tr([html.Td(['x',html.Sup(3),html.Td(id='cube')])]),
+        html.Tr([html.Td(['2',html.Sup('x'),html.Td(id='twos')])]),
+        html.Tr([html.Td(['3',html.Sup(3),html.Td(id='three')])]),
+        html.Tr([html.Td(['x',html.Sup('x'),html.Td(id='x^x')])]),
+    ])
+])
+
+
 if __name__ == '__main__':
 
     app.run(debug=True)
