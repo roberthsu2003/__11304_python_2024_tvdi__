d28ae8a149b75db83ddbe22d21bbee4dd48c6fe7
roberthsu2003 <roberthsu2003@gmail.com>
2024-07-30 16:32:59 +0800

    modify

--- 2024_07_30_flask_dash/auth/main.py
+++ 2024_07_30_flask_dash/auth/main.py
@@ -2,6 +2,7 @@ from flask import Blueprint,render_template,request
 from flask_wtf import FlaskForm
 from wtforms import EmailField,PasswordField
 from wtforms.validators import DataRequired,Length
+from .datasource import validateUser
 auth_blueprint = Blueprint('auth',__name__)
 
 class LoginForm(FlaskForm):
@@ -21,6 +22,12 @@ def index():
             password = form.password.data
             print(f'email:{email}')
             print(f'password:{password}')
+            is_ok, username = validateUser(email,password)
+            if is_ok:
+                print(f"您好:{username}")
+            else:
+                print(f'密碼錯誤')
+
     else:
         print("這是第一次進入")
     
