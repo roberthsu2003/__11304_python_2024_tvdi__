d1e4306ee11dfa7e9d25eeb61f39783dad3702c0
roberthsu2003 <roberthsu2003@gmail.com>
2024-07-30 14:34:37 +0800

    modify

--- 2024_07_30_flask_dash/auth/main.py
+++ 2024_07_30_flask_dash/auth/main.py
@@ -1,8 +1,16 @@
 from flask import Blueprint,render_template
+from flask_wtf import FlaskForm
+from wtforms import EmailField,PasswordField
+from wtforms.validators import DataRequired,Length
 auth_blueprint = Blueprint('auth',__name__)
+
+class LoginForm(FlaskForm):
+    email = EmailField('郵件信箱',validators=[DataRequired()])
+    PasswordField('密碼',validators=[DataRequired(),Length(min=4, max=20)])
 
 
 @auth_blueprint.route("/auth/",methods=['GET', 'POST'])
 @auth_blueprint.route("/auth/login",methods=['GET', 'POST'])
 def index():
-    return render_template('/auth/login.html.jinja')
+    form = LoginForm()
+    return render_template('/auth/login.html.jinja',form=form)
