dcf06a64210f04dc57beb01f76d4cb5ec5884b04
roberthsu2003 <roberthsu2003@gmail.com>
2024-07-30 11:29:37 +0800

    main

--- 2024_07_30_flask_dash/auth/main.py
+++ 2024_07_30_flask_dash/auth/main.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 auth_blueprint = Blueprint('auth',__name__)
-
 @auth_blueprint.route("/auth/")
+@auth_blueprint.route("/auth/login")
 def index():
     return "<h1>我是auth的首頁</h1>"
